fa7a14cf7459d47cce56d04fb8901bf978e65421
Magnus <mp-se@noreply.github.com>
2024-07-08 07:57:13 +0200

    Ignoring api when check is disabled

--- service-api/app/api/security.py
+++ service-api/app/api/security.py
@@ -5,9 +5,10 @@ from .config import get_settings
 
 logger = logging.getLogger(__name__)
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token", auto_error=False)
 
 def api_key_auth(api_key: str = Depends(oauth2_scheme)):
+    logger.info("Validating access token")
     settings = get_settings()
     if settings.api_key_enabled == True:
         if api_key != settings.api_key:
